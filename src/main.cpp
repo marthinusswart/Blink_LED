91f6d5dc78cb4ab248efb706a0f2165d2d00e76c
Marthinus Swart <marthinus.swart@gmail.com>
2025-11-28 21:37:28 +1100

    Refactor sensor reading logic and improve button handling; add Ticker for periodic sensor reads

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include <Ticker.h>
 
 #define GREEN_LED_PIN 5   // D1 = GPIO5
-#define YELLOW_LED_PIN 14 // D5 = GPI14
-#define RED_LED_PIN 12    // D6 = GPI12
-#define READ_DELAY_MS 5000
+#define YELLOW_LED_PIN 14 // D5 = GPIO14
+#define RED_LED_PIN 12    // D6 = GPIO12
+#define READ_DELAY_SEC 10 // Read every 10 seconds
 #define BLINK_DELAY_MS 2000
 #define DHT_PIN 4 // D2 = GPIO4
 #define DHTTYPE DHT22
+#define BUTTON_PIN 2 // D4 = GPIO2
 DHT dht(DHT_PIN, DHTTYPE);
 
 #ifdef DEBUG
@@ -20,6 +22,11 @@ DHT dht(DHT_PIN, DHTTYPE);
 
 void setIndicator(float temperature);
 void resetIndicators();
+void sensorReadCallback();
+
+Ticker sensorTimer;
+int buttonValue;
+volatile bool readSensor = false;
 
 void setup()
 {
@@ -32,32 +39,54 @@ void setup()
     pinMode(YELLOW_LED_PIN, OUTPUT);
     pinMode(RED_LED_PIN, OUTPUT);
     dht.begin();
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    DBG("BUTTON_PIN = %d\n", BUTTON_PIN);
     DBG("GREEN_LED_PIN = %d\n", GREEN_LED_PIN);
     DBG("YELLOW_LED_PIN = %d\n", YELLOW_LED_PIN);
     DBG("RED_LED_PIN = %d\n", RED_LED_PIN);
+
+    sensorTimer.attach(READ_DELAY_SEC, sensorReadCallback); // Every 5s
 }
 
 void loop()
 {
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-    if (isnan(h) || isnan(t))
+    // Check button for immediate reading
+    static unsigned long lastDebounceTime = 0;
+    unsigned long now = millis();
+    if (digitalRead(BUTTON_PIN) == LOW && (now - lastDebounceTime > 200))
     {
-        DBGL("Failed to read from DHT sensor!");
-    }
-    else
-    {
-        DBGL("Humidity: %.1f%%  Temperature: %.1f *C ", h, t);
+        DBGL("Button: LOW!");
+        lastDebounceTime = now;
+        readSensor = true;
     }
 
-    setIndicator(t);
-    delay(BLINK_DELAY_MS);
-    resetIndicators();
-    delay(READ_DELAY_MS);
+    // Small delay
+    delay(100);
+
+    if (readSensor)
+    {
+        readSensor = false;
+        float h = dht.readHumidity();
+        float t = dht.readTemperature();
+
+        if (isnan(h) || isnan(t))
+        {
+            DBGL("Failed to read from DHT sensor!");
+        }
+        else
+        {
+            DBGL("Humidity: %.1f%%  Temperature: %.1f *C ", h, t);
+        }
+
+        setIndicator(t);
+        delay(BLINK_DELAY_MS);
+        resetIndicators();
+    }
 }
 
 void setIndicator(float temperature)
 {
+    DBGL("Setting indicators for temperature: %.1f", temperature);
     if (temperature <= 25.0)
     {
         digitalWrite(GREEN_LED_PIN, HIGH); // LED on
@@ -83,4 +112,9 @@ void resetIndicators()
     digitalWrite(GREEN_LED_PIN, LOW);  // LED off
     digitalWrite(YELLOW_LED_PIN, LOW); // LED off
     digitalWrite(RED_LED_PIN, LOW);    // LED off
+}
+
+void sensorReadCallback()
+{
+    readSensor = true;
 }
